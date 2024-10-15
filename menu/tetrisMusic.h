929be4f1f9ada5dcd1ae76d132aa225a3fd18bad
sibi12325 <136464996+sibi12325@users.noreply.github.com>
2024-10-15 17:26:54 +0800

    Delete menu directory

--- menu/tetrisMusic.h
+++
@@ -1,9 +0,0 @@
- /* 
-  Tetris theme - (Korobeiniki) 
-  Connect a piezo buzzer or speaker to pin 11 or select a new pin.
-  More songs available at https://github.com/robsoncouto/arduino-songs                                            
-                                              
-                                              Robson Couto, 2019
-*/
-
-void playTetrisMusic();  // Declaration of the function
